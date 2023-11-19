c2d93e7535507fd561f40917bb22322f629e3fd7
Philip Dhingra <195001+philipkd@users.noreply.github.com>
2023-11-19 14:42:05 -0800

    added a database index

--- db/views.py
+++ db/views.py
@@ -1,9 +1,48 @@
-import os, marko, glob
+import os, marko, glob, re
+import pandas as pd
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
 
+
+class Database:
+
+	@staticmethod
+	def get_tags(file):
+	    tags = list(map(lambda x: x[0],re.findall(r'#(.*?)([\. ]|$)',file)))
+	    if (re.search(r'/- ',file)):
+	        tags.append('_stbd')
+	    return tags		
+
+	def __init__(self):
+
+		NOTES_DIR = str(settings.BASE_DIR) + "/_external/content/db3/files/"
+		files = glob.glob(str(NOTES_DIR) + '**/*.txt', recursive=True)
+
+		df = pd.DataFrame(data=files,columns=['file'])
+		df['file'] = df['file'].str.replace(NOTES_DIR,'')
+		df['tags'] = df['file'].apply(self.get_tags)
+
+		self.df = df[df.apply(lambda x: False if '_pi' in x['tags'] else True,axis=1)]
+
+	def __tag_counts(self):
+		tag_counts = pd.Series([item for sublist in self.df['tags'] for item in sublist]).value_counts().to_frame(name='count').reset_index()
+		tag_counts = tag_counts.rename(columns={'index':'tag'})
+		return tag_counts
+
+	def basic_tag_counts(self):
+		tag_counts = self.__tag_counts()
+		return tag_counts[~tag_counts.tag.str.contains('^_')]
+
+	def special_tag_counts(self):
+		tag_counts = self.__tag_counts()
+		print(tag_counts)
+		return tag_counts[tag_counts.tag.str.contains('^_')]
+
+
 def index(request):
+
+	db = Database()
 
 	files = glob.glob(str(settings.BASE_DIR) + '/_external/content/preview/**/*.txt', recursive=True)
 
@@ -20,12 +59,8 @@ def index(request):
 	body = marko.convert("\n".join(bodies))
 
 	context = {
-	    "author": "Gaurav Singhal",
-	    "website": {
-	        "domain": "https://pluralsight.com",
-	        "views": 200
-	    },
-	    "odds": [1, 3, 5],
+	    "special_tag_counts": db.special_tag_counts(),
+	    "basic_tag_counts": db.basic_tag_counts(),
 	    "body": 'yo'
 	}
 
