b62b17a2bd1934411ef1cd73f49f1f9445e441e8
Philip Dhingra <195001+philipkd@users.noreply.github.com>
2023-11-21 14:57:27 -0800

    tags use url built-in

--- db/urls.py
+++ db/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("tag/<str:tag>/", views.tag, name="tag")
+    path("tag/<str:tag>", views.tag, name="tag")
 ]
